5bcb842a326104d93913f45f6244e511485e29ee
zph <1305353123@qq.com>
2021-08-22 00:03:02 +0800

    创建项目

---
+++ src/test/java/com/it/designpattern/pattern/DesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.designpattern.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
