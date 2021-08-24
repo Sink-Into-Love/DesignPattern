45fc69e84c048366a6a9756ff909d0de721c451f
zph <1305353123@qq.com>
2021-08-24 11:10:14 +0800

    单一职责原则

--- src/main/java/com/it/designpattern/principles/SingleResponsibility/SR2.java
+++ src/main/java/com/it/designpattern/principles/SingleResponsibility/SR2.java
@@ -3,6 +3,9 @@ package com.it.designpattern.principles.SingleResponsibility;
 /**
  * @author zph
  * 单一职责原则
+ * 对类来说的，即一个类应该只负责一项职责。
+ * 如类 A 负责两个不同职责：职责 1，职责 2。
+ * 当职责 1 需求变更 而改变 A 时，可能造成职责 2 执行错误，所以需要将类 A 的粒度分解为 A1，A2
  */
 public class SR2 {
 
